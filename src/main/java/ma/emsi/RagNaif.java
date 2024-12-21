b7a2fa7e419ff82f068e041b06ed5daddf234d2f
YounesEZ <ezbidayounes@gmail.com>
2024-12-21 17:09:48 +0100

    Test3

--- src/main/java/ma/emsi/RagNaif.java
+++ src/main/java/ma/emsi/RagNaif.java
@@ -7,8 +7,6 @@ import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
-import dev.langchain4j.data.embedding.Embedding;
-import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
@@ -30,21 +28,36 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.Scanner;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class RagNaif {
+
+    private static void configureLogger() {
+        // Configure le logger sous-jacent (java.util.logging)
+        Logger packageLogger = Logger.getLogger("dev.langchain4j");
+        packageLogger.setLevel(Level.FINE); // Ajuster niveau
+        // Ajouter un handler pour la console pour faire afficher les logs
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.FINE);
+        packageLogger.addHandler(handler);
+    }
+
     public static void main(String[] args) {
+
+        configureLogger();
 
         ChatLanguageModel modele = GoogleAiGeminiChatModel.builder()
                 .apiKey(System.getenv("GEMINI_KEY"))
                 .modelName("gemini-1.5-flash")
                 .temperature(0.7)
+                .logRequestsAndResponses(true)
                 .build();
 
-        //Phase 1
-        //Etape1
+
         Path pathRessource;
         try {
             String cheminRessource = "/ml.pdf"; // chemin absolu
@@ -55,28 +68,23 @@ public class RagNaif {
             throw new RuntimeException(e); // ou un autre traitement du problème...
         }
 
-        //Etape2
         DocumentParser documentParser = new ApacheTikaDocumentParser();
 
-        //Etape3
         Document document = FileSystemDocumentLoader.loadDocument(pathRessource, documentParser);
 
-        // Étape 4 : Découper le document en morceaux (chunks)
         DocumentSplitter splitter = DocumentSplitters.recursive(300, 30);
 
-        //Etape5
         EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
 
-        //Etape6
+        //Etape6 Phase1 Test1
         //explication : le code ne veux pas fonctionner sans ingerer le document dans le EmbeddingStore c'est pour cela que j'ai crée
         //               un EmbeddingStoreIngestor avec le pattern Builder en lui donnant le Splitter , le model d'embedding ainsi que
         //               le EmbeddingStore
 
-        //Phase 2
-        //Etape 1
+
         EmbeddingStore embeddingStore = new InMemoryEmbeddingStore();
 
-        // Creation du EmbeddingStoreIngestor
+
         EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
                 .embeddingStore(embeddingStore)
                 .embeddingModel(embeddingModel)
@@ -101,17 +109,17 @@ public class RagNaif {
                 .queryTransformer(transformer)
                 .build();
 
-        //Etape 2
-        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10); // n'est pas utilisé pour cet exercice
 
-        //Etape3
+        ChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+
+
         Assistant assistant = AiServices.builder(Assistant.class)
                 .chatLanguageModel(modele)
                 .chatMemory(chatMemory)
                 .retrievalAugmentor(augmentor)
                 .build();
 
-        //Etape4
+
         try (Scanner scanner = new Scanner(System.in)) {
             while (true) {
                 System.out.println("==================================================");
